a55df70b12d3ee6c9490987c6b9943d51c369a2f
Vladimir Filippov <vladimirfilippov@yandex.ru>
2022-02-23 20:52:39 +0300

    feat: add open popup form-card script

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,45 @@
-const popupElement = document.querySelector('.popup');
-const popupOpenButtonElement = document.querySelector('.profile__button-edit');
-const profileTitleElement = document.querySelector('.profile__title');
-const profileSubtitleElement = document.querySelector('.profile__subtitle');
-const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
-// Находим форму в DOM
-const formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
-const nameInput = formElement.querySelector('.popup__input_type_name');
-const jobInput = formElement.querySelector('.popup__input_type_job');
+// Находим попап редактирования имени и профессии
+const popupEditElement = document.querySelector(".popup_edit");
+const popupOpenButtonElement = document.querySelector(".profile__button-edit");
+const profileTitleElement = document.querySelector(".profile__title");
+const profileSubtitleElement = document.querySelector(".profile__subtitle");
+const popupCloseButtonElement = popupEditElement.querySelector(".popup__button-close");
+// Находим форму редактирования имени и профессии в DOM
+const formElement = popupEditElement.querySelector(".popup__form_name");
+// Находим поля формы редактирования имени и профессии в DOM
+const nameInput = formElement.querySelector(".popup__input_type_name");
+const jobInput = formElement.querySelector(".popup__input_type_job");
 
-const openPopup = function() {
-  popupElement.classList.add('popup_opened');
+const openPopup = function () {
+  popupEditElement.classList.add("popup_opened");
   nameInput.value = profileTitleElement.textContent;
   jobInput.value = profileSubtitleElement.textContent;
 };
 
-const closePopup = function() {
-  popupElement.classList.remove('popup_opened');
+const closePopup = function () {
+  popupEditElement.classList.remove("popup_opened");
+};
+
+// Находим попап добавления карточки
+const popupAddCardElement = document.querySelector(".popup_add");
+const popupAddCardButtonElement = document.querySelector(".profile__button-add");
+const popupCloseAddCardButtonElement = popupAddCardElement.querySelector(".popup__button-close");
+// Находим форму добавления карточки в DOM
+const formAddElement = popupAddCardElement.querySelector(".popup__form_card");
+
+const openAddCardPopup = function () {
+  popupAddCardElement.classList.add("popup_opened");
+};
+
+const closeAddCardPopup = function () {
+  popupAddCardElement.classList.remove("popup_opened");
 };
 
 // Регистрируем обработчики событий по клику
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
+popupOpenButtonElement.addEventListener("click", openPopup);
+popupCloseButtonElement.addEventListener("click", closePopup);
+popupAddCardButtonElement.addEventListener("click", openAddCardPopup);
+popupCloseAddCardButtonElement.addEventListener("click", closeAddCardPopup);
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -40,6 +58,17 @@ function handleProfileFormSubmit(evt) {
   closePopup();
 }
 
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleProfileFormAddCard(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+
+  closeAddCardPopup();
+}
+
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", handleProfileFormSubmit);
+formAddElement.addEventListener("submit", handleProfileFormAddCard);
