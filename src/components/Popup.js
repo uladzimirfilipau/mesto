f55c29a615d723fbe6994aa27b63500245c08d8c
Vova <uladzimirfilipau@gmail.com>
2022-05-08 19:18:14 +0300

    add preloader

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,12 +4,15 @@ export class Popup {
   // Принимает в конструктор единственный параметр — селектор попапа.
   constructor(popupSelector) {
     this._popup = popupSelector;
+    this._form = this._popup.querySelector(".popup__form");
+    this._buttonText = this._popup.querySelector(".popup__button-text");
     this._closeButton = this._popup.querySelector(".popup__button-close");
+
     this._handleEscClose = this._handleEscClose.bind(this);
     this._handleOverlayClose = this._handleOverlayClose.bind(this);
     this.close = this.close.bind(this);
   }
-  // Содержит публичные методы open и close,
+  // Создать публичные методы open и close,
   // которые отвечают за открытие и закрытие попапа.
   open() {
     this._popup.classList.add("popup_opened");
@@ -20,25 +23,26 @@ export class Popup {
     this._popup.classList.remove("popup_opened");
     document.removeEventListener("keydown", this._handleEscClose);
   }
-  // Содержит приватный метод _handleEscClose,
+  // Создать приватный метод _handleEscClose,
   // который содержит логику закрытия попапа клавишей Esc.
   _handleEscClose(evt) {
     if (evt.key === ESC_CODE) {
       this.close();
     }
   }
-  // Содержит приватный метод _handleOverlayClose,
+  // Создать приватный метод _handleOverlayClose,
   // который содержит логику закрытия попапа по клику на оверлее.
   _handleOverlayClose(evt) {
     if (evt.target.classList.contains("popup_opened")) {
       this.close();
     }
   }
-  // Содержит публичный метод setEventListeners,
+  // Создать публичный метод setEventListeners,
   // который добавляет слушатель клика по иконке закрытия попапа.
   setEventListeners() {
     this._closeButton.addEventListener("click", this.close);
-    // Модальное окно также закрывается при клике на затемнённую область вокруг формы.
+    // Модальное окно также закрывается при клике
+    // на затемнённую область вокруг формы.
     this._popup.addEventListener("mousedown", this._handleOverlayClose);
   }
 }
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,10 +4,24 @@ export class PopupWithForm extends Popup {
   // Класс принимает в конструктор: селектор попапа и колбэк сабмита формы
   constructor(popupSelector, { handleFormSubmit }) {
     super(popupSelector);
+    
     this._handleFormSubmit = handleFormSubmit;
-    this._form = this._popup.querySelector(".popup__form");
     this._inputList = this._form.querySelectorAll(".popup__input");
+
+    this._savingButtonText = document
+    .querySelector(".text__template")
+    .content.querySelector(".popup__button-text_saving")
+    .cloneNode(true);
+    
   }
+  // Создать прелоадер
+  renderLoading(isLoading) {
+    if (isLoading) {
+      this._buttonText.replaceWith(this._savingButtonText);
+    } else {
+      this._savingButtonText.replaceWith(this._buttonText);
+    }
+  };
   // Создать приватный метод _getInputValues,
   // который собирает данные всех полей формы
   _getInputValues() {
